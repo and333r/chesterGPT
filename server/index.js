25284a5e663170413fc987d2bd9f8459dfb8bae3
Ander <andercaroescan9@gmail.com>
2023-05-05 17:23:33 +0200

    prueba

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001
 const app = express()
 
 app.use(express.static(path.resolve(__dirname, '../chester/build')));
-
+//Hola me llamo Ander
 
 app.get("/api", (req,res)=>{
     res.json({message:"Hola desde el servidor!"})
