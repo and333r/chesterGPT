3559dd97bf72625fd9b2d275c08451fbc26475c0
Ander Caro <113524473+and333r@users.noreply.github.com>
2023-05-05 17:08:06 +0200

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -12,9 +12,19 @@ app.get("/api", (req,res)=>{
     res.json({message:"Hola desde el servidor!"})
 })
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../chester/build', 'index.html'));
   });
+
+app.get('/java', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../chester/build', 'java.html'));
+});
+app.get('/python', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../chester/build', 'python.html'));
+});
+app.get('/js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../chester/build', 'js.html'));
+});
 
 app.listen(PORT,()=>{
     console.log(`Server listening on ${PORT}`)
